84a915d77634d3b8f0746518d2ca091cc9408fd6
Dipraj Howlader <diprajhowlader2015@gmail.com>
2021-08-22 22:04:48 +0600

    some change

--- VariableAndData.java
+++ VariableAndData.java
@@ -3,6 +3,20 @@ package JavaPhase;
 public class VariableAndData {
 	
 	public static void main(String[] args) {
-	
+		System.out.println("Hello Developers\n");
+	 int Dipraj = 12;
+	 System.out.println(Dipraj);
+	 double d = 125.2;
+	 System.out.println(d);
+	 boolean B= true;
+	 System.out.println(B);
+	 float fa = (float) 655.02;	
+	 System.out.println(fa);
+	 char c = 'd';
+	 System.out.println(c);
+	 short s = 125;
+	 System.out.println(s);
+	 System.out.println("This is whole = "+d+ B + fa + c + s + Dipraj);
+	 
 }
 }
